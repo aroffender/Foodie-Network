91b16dee13903c1d9ae775661974090d45997378
Aronno Rahman <59930425+aroffender@users.noreply.github.com>
2024-06-13 02:43:27 +0600

    Update 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async
+{
+
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
+
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Foodie Network',
       theme: ThemeData(brightness: Brightness.light,
         colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(160, 50, 234, 179)),
         useMaterial3: true,
       ),
       darkTheme: ThemeData(brightness: Brightness.dark),
       themeMode: ThemeMode.system,
-      home: const MyHomePage(title: 'Home Page'),
+      home: Scaffold(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button too many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
