caa4f91b8a7bef97d3f86dc7a454738d4ecbcf55
aroffender <mohammadaronno@gmail.com>
2024-06-22 22:32:09 +0600

    Merge remote-tracking branch 'origin/main'

--- lib/authentication/authtentication_screen.dart
+++ lib/authentication/authtentication_screen.dart
@@ -6,13 +6,76 @@ class Auth extends StatefulWidget {
   State<Auth> createState() => _AuthState();
 }
 
-class _AuthState extends State<Auth> {
+class AuthState extends State<Auth> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Welcome"),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.cyan,
+                  Colors.amber,
+                ],
+                begin: const FractionalOffset(0.0, 0.0),
+                end: const FractionalOffset(1.0, 0.0),
+                stops: [0.0, 1.0],
+                tileMode: TileMode.clamp,
+              ),
+            ),
+          ),
+          title: const Text(
+            'Auth',
+            style: TextStyle(
+              fontSize: 60,
+              color: Colors.white,
+              fontFamily: "Lobster",
+            ),
+          ),
+          centerTitle: true,
+          bottom: const TabBar(
+            indicatorColor: Colors.white10,
+            indicatorWeight: 6,
+            tabs: [
+              Tab(
+                icon: Icon(Icons.lock, color: Colors.white),
+                text: "Login",
+              ),
+              Tab(
+                icon: Icon(Icons.person, color: Colors.white),
+                text: "Register",
+              ),
+            ],
+          ),
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Colors.amber,
+                Colors.cyan,
+              ],
+            ),
+          ),
+          child: const TabBarView(
+            children: [
+              Center(child: Text('Login Screen')),
+              Center(child: Text('Register Screen')),
+            ],
+          ),
+        ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: Auth(),
+  ));
 }
