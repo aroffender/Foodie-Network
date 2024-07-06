00011fb2b2e42279030b2d5f9158e25b248090e5
Azinkia Prince <90772438+Azinkia-Prince@users.noreply.github.com>
2024-07-06 22:07:49 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,11 @@ Future<void> main() async
   WidgetsFlutterBinding.ensureInitialized();
   Firebase.initializeApp();
   //await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 
-
-
-
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
